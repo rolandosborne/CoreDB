3e93dbe70f8df86a4c6ed012cdca9a7714fd938a
Roland Osborne <roland.osborne@gmail.com>
2021-12-11 21:55:36 -0800

    renamed src dir

---
+++ app/go.mod
@@ -0,0 +1,5 @@
+module github.com/rolandosborne/coredb
+
+go 1.17
+
+require github.com/go-chi/chi v1.5.4 // indirect
--- app/main.go
+++ app/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+  "fmt"
 	"net/http"
 )
 
 func main() {
-
+  fmt.Println("listening on port 8080");
 	http.ListenAndServe(":8080", ChiRouter().InitRouter())
 }
